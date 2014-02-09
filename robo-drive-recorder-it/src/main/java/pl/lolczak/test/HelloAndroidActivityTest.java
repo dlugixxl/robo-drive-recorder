08484bd5c6b6e27241395bd42c104be04a52beba
lolczak <lukasz.jan.olczak@gmail.com>
2014-02-09 13:46:39 +0100

    Project initation from archetype

---
+++ robo-drive-recorder-it/src/main/java/pl/lolczak/test/HelloAndroidActivityTest.java
@@ -0,0 +1,17 @@
+package pl.lolczak.test;
+
+import android.test.ActivityInstrumentationTestCase2;
+import pl.lolczak.*;
+
+public class HelloAndroidActivityTest extends ActivityInstrumentationTestCase2<HelloAndroidActivity> {
+
+    public HelloAndroidActivityTest() {
+        super(HelloAndroidActivity.class); 
+    }
+
+    public void testActivity() {
+        HelloAndroidActivity activity = getActivity();
+        assertNotNull(activity);
+    }
+}
+
